1e4213848d34f33bd749ec75c85c7dff3069e6ff
nakoshmario <surikgoodlike@mail.ru>
2024-02-27 21:14:55 +0300

    newtest

---
+++ classpython.py
@@ -0,0 +1,48 @@
+
+
+# class human:
+#     def __init__(self, fullName="",birthDay="",phone="",city="",adress="",country=""):
+#         self.fullName = fullName
+#         self.birthDay = birthDay
+#         self.phone = phone
+#         self.city= city
+#         self.adress = adress
+#         self.country = country
+
+#     def input_data(self):
+#         self.fullName = input("введите ФИО: ")
+#         self.birthDay = input("Введите дату рождения: ")
+#         self.phone = input("Введите номер телефона: ")
+#         self.city = input("Введите город проживания: ")
+#         self.adress = input("Введите адрес проживания: ")
+#         self.country = input("Введите страну проживания: ")
+#     def display_data(self):
+#         print('ФИО: ',self.fullName)
+#         print('Дата рождения: ',self.birthDay)
+#         print('Телефон: ',self.phone)
+#         print('Город проживания: ',self.city)
+#         print('Адрес проживания: ',self.adress)
+#         print('Страна проживания: ',self.country)
+# human = human()
+# human.input_data()
+# print('--------------------')
+# human.display_data()
+
+# class soda:
+#     def __init__(self,gazirovka="обычная водичка"):
+#         self.gazirovka = gazirovka
+   
+
+#     def show_my_drink(self):
+#         print('газировка: ',self.gazirovka)
+
+
+
+# sod1 = soda()
+# sod1.show_my_drink() 
+
+
+
+
+
+
--- holodilnik.py
+++ holodilnik.py
@@ -1,35 +1,35 @@
 # функция добавления продукта в холодильник
-import datetime
-Date_D = '%Y-%m-%d'
-goods = {
-    'пельмени':[
-        {'amount': 1, 
-        'expiration_date': datetime.date(2023, 9, 30)
-    },]
-}
-def add(goods,title,amount,expriration_date=None):
-    holod=goods.keys()
-    if expriration_date != None:
-        data_test = datetime.datetime.strptime(expriration_date,Date_D)
-        dataD = data_test.date()
-        if  title in holod :
-            goods[title].append({'amount': amount,'expiration_date':dataD})
-        else:
-            goods[title]=[{'amount':amount,'expriration_date':dataD}]
-    else:
-       if title in holod :
-           goods[title].append({'amount':amount,'expiration_date':expriration_date})
-       else:
-           goods[title]=[{'amount':amount,'expiration_date':expriration_date}]
+# import datetime
+# Date_D = '%Y-%m-%d'
+# goods = {
+#     'пельмени':[
+#         {'amount': 1, 
+#         'expiration_date': datetime.date(2024, 2, 27)
+#     },]
+# }
+# def add(goods,title,amount,expriration_date=None):
+#     holod=goods.keys()
+#     if expriration_date != None:
+#         data_test = datetime.datetime.strptime(expriration_date,Date_D)
+#         dataD = data_test.date()
+#         if  title in holod :
+#             goods[title].append({'amount': amount,'expiration_date':dataD})
+#         else:
+#             goods[title]=[{'amount':amount,'expriration_date':dataD}]
+#     else:
+#        if title in holod :
+#            goods[title].append({'amount':amount,'expiration_date':expriration_date})
+#        else:
+#            goods[title]=[{'amount':amount,'expiration_date':expriration_date}]
 
 
-add(goods,"пельмени-детские",5, '2023-3-12' )
-add(goods,"пельмени",4)
-add(goods,'курица',4,   '2024-8-17')
-add(goods,'квас',6)
+# add(goods,"пельмени-детские",5, '2023-3-30' )
+# add(goods,"пельмени",4)
+# add(goods,'курица',4,   '2024-8-17')
+# add(goods,'квас',6)
 # # print(goods)
 # def add_by_note(items,note):
-#     key=goods.key()
+#     key=goods.keys()
 #     spisok = title.split(" ")
 #     stroka= spisok[-1]
 #     if stroka.find('-') != -1:
@@ -43,25 +43,55 @@ add(goods,'квас',6)
 #         else:
 #             num = float(spisok[-1])
 #             amount = num
+            # print(amount)
 
                 
 
 
 
 # поиск продуктов 
-def search(goods,title):
-    for i in goods:
-        if i == title:
-            return goods[i]
+# def search(goods,title):
+#     for i in goods:
+#         if i == title:
+#             return goods[i]
         
-print(search(goods,'курица'))
+# print(search(goods,'курица'))
 
 # поиск количества продукта
 
-def findAmount(goods,title):
+# def findAmount(goods,title):
 
-    for i in goods:
-        if i == title:
-            return (goods,i['amount'])
-    print(findAmount(goods,'курица'))
+#     for i in goods:
+#         if i == title:
+#             return (goods,i['amount'])
+#     print(findAmount(goods,'курица'))
+    # функция удаления продукта
+# def deleteGoodFromList(goods,name):
+#     goods.remove(name)
+#     return goods
+# print(deleteGoodFromList(goods,'курица'))
+
+# функция проверки на срок годности
+# def  checkExpire(item,in_advanca_days):
+#     res  = []
+#     now = datetime.date.today()
+#     amountD = datetime.timedelta(days = in_advanca_days)
+   
+#     print('------')
+#     print('срок истечет в течении',amountD )
+#     print('------')
+#     print('сенодняшняя дата',now)
+#     print('------')
+#     print('дней до просрочки')
+#     print('------')
+
+#     for i in item:
+#         for j in item[i]:
+#             if j ['expiration_date'] != None:
+#                 print('продукты которые истекают',j['expiration_date']- now)
+#                 if (j['expiration_date'] - now) <= amountD:
+#                     findAmount(item,i)
+#                     list.append(res,(i,findAmount(item,i)))
+#     return res
+# print(checkExpire(goods,2))
     
